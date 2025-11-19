862be1092925c9876721eec65052c88de5ed60b4
roujon2 <jonroulet@gmail.com>
2025-11-19 13:19:13 -0300

    score and retry menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,8 @@ extends Node
 
 @export var mob_scene: PackedScene
 
-
+func _ready():
+	$UserInterface/Retry.hide()
 
 func _on_mob_timer_timeout() -> void:
 	# Create a new instance of the Mob scene.
@@ -19,7 +20,15 @@ func _on_mob_timer_timeout() -> void:
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
-
+	
+	# We connect the mob to the score label to update the score upon squashing one.
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
 
 func _on_player_hit() -> void:
 	$MobTimer.stop() 
+	$UserInterface/Retry.show()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# Restarts the current scene
+		get_tree().reload_current_scene()
